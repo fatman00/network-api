dfc3de2f5dbb3e54c3336a4c8f6f2d39300b5514
fatman00 <32018179+fatman00@users.noreply.github.com>
2023-11-28 15:26:22 +0100

    Create get_device_interfaces_ip.py

---
+++ app/endpoints/get_device_interfaces_ip.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException
+from ..std_errors import CLIENT_NOT_REACHABLE_404, UNAUTHORIZED_401
+
+from napalm import get_network_driver
+from netmiko import NetmikoAuthenticationException
+from napalm.base.exceptions import ConnectionException
+import yaml
+
+# Instantiate an instance of APIRouter
+router = APIRouter()
+
+# Define a API endpoint for getting device facts.
+@router.get(
+    "/{hostname}/interfaces_ip",
+    summary="Retrieve device interfaces ip addresses",
+)
+def get_running_config(hostname: str):
+
+    # Read local Inventory file
+    with open("app/device_inventory.yaml", "r") as f:
+        device_inventory = yaml.safe_load(f)
+
+    # Select target device from Inventory file
+    target_device = device_inventory[hostname]
+
+    # Define the NAPALM driver and the device connection parameters
+    driver = get_network_driver(target_device["device_type"])
+    device = driver(
+        target_device["ip"], target_device["username"], target_device["password"]
+    )
+
+    # Attempt to connect to device and handle any exceptions.
+    try:
+        device.open()
+    except NetmikoAuthenticationException as e:
+        raise HTTPException(status_code=401, detail=UNAUTHORIZED_401) from e
+    except ConnectionException as e:
+        raise HTTPException(status_code=404, detail=CLIENT_NOT_REACHABLE_404) from e
+
+    # If connection was successful, return the device interfaces.
+    else:
+        return device.get_interfaces_ip()
